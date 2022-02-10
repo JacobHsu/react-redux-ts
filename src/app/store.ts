5312ac4db0542c57b5baa2ea4eca5432e78cc00d
jacobhsu <jacob.hsu.tw@gmail.com>
2022-02-10 10:13:46 +0800

    combineReducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,14 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import decrementReducer from '../features/counter/decrementSlice';
 
+const reducers = combineReducers({
+  counter: counterReducer,
+  decrement: decrementReducer,
+});
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    decrement: decrementReducer,
-  },
+  reducer: reducers,
 });
 
 export type AppDispatch = typeof store.dispatch;
