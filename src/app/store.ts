584346d6c2cabfdffd5b001b2ecf52cb248e8115
jacobhsu <jacob.hsu.tw@gmail.com>
2022-02-10 11:13:53 +0800

    feat: redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,29 @@
 import { combineReducers, configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import counterReducer from '../features/counter/counterSlice';
 import decrementReducer from '../features/counter/decrementSlice';
 
-const reducers = combineReducers({
-  counter: counterReducer,
-  decrement: decrementReducer,
-});
+// https://github.com/rt2zz/redux-persist
+
+const persistConfig = {
+  key: 'primary',
+  storage,
+}
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
+    counter: counterReducer,
+    decrement: decrementReducer,
+  })
+);
+
 
 export const store = configureStore({
-  reducer: reducers,
+  reducer: persistedReducer,
 });
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
